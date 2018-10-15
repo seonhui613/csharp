0b9d9e029f551dd9cf9ab2c3db18d9dadba77a78
seonhui <h613@naver.com>
2018-10-15 15:28:27 +0900

    second

---
+++ HelloWorld/TypeCastExample1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCastExample1
+{
+    class TypeCastExample1
+    {
+        public static void Main()
+        {
+            short a = 10;    //                             0000 0000 0000 1010
+            int b = a;        // 0000 0000 0000 0000 0000 0000 0000 1010
+            int c = 50000;  // 0000 0000 0000 0000 1100 0011 0101 0000
+            try
+            {
+                short d = checked( (short) c );     //                      1100 0011 0101 0000
+            } catch (Exception e) {
+                //Console.WriteLine("예외 : {0}", e.StackTrace);  //Short :-15536  (맨첫 글자가 1이니까 음수.
+                Console.WriteLine("예외 : {0}" + e.StackTrace); 
+            }
+        }
+    }
+}
---
+++ HelloWorld/TypeCastExample2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeCastExample2
+{
+    class TypeCastExample2
+    {
+        public static void Main(string[] args)
+        {
+            int iSize = sizeof(int);
+            Console.WriteLine("int형 바이트 길이 :{0}", iSize); // int형 바이트 길이 :4
+
+            Type myType1 = typeof(int);
+            Console.WriteLine("typeof(int) :{0}", myType1); // typeof(int) :System.Int32
+
+            int i = 10;
+            Type myType2 = i.GetType();
+            Console.WriteLine("i.GetType() :{0}", myType2); // i.GetType() :System.Int32
+
+            int j = 90;
+            Console.WriteLine("Size of j :{0}", Marshal.SizeOf(j)); // Size of j :4
+        }
+    }
+}
